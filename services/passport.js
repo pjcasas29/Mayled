1afc5ae9cf61b091317f86c4fa51120b8f47ffd6
pjcasas29 <pjcasas29@hotmail.com>
2020-02-06 13:40:54 -0500

    Keys typo again

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const keys = require("./../config/keys");
-
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 
