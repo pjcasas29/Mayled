6d9e5343afc4a271f63d7e43f042536693ba70aa
pjcasas29 <pjcasas29@hotmail.com>
2020-02-06 13:28:55 -0500

    Keys typo

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  googleCLientId: process.enc.GOOGLE_CLIENT_ID,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
