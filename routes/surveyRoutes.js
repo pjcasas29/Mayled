43dbab42238d3956cc37e77dde2cbb5d64a8f85a
pjcasas29 <pjcasas29@hotmail.com>
2020-02-11 18:40:48 -0500

    Mailer template and update database

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,7 +6,7 @@ const Survey = mongoose.model("surveys");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -17,7 +17,16 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+    try {
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
 
-    const mailer = new Mailer(survey, surveyTemplate(survey));
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(err);
+    }
   });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("no-reply@emaily.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
@@ -13,6 +14,7 @@ class Mailer extends helper.Mail {
 
     this.addContent(this.body);
     this.addClickTracking();
+    this.addRecipients();
   }
 
   formatAddresses(recipients) {
@@ -27,6 +29,26 @@ class Mailer extends helper.Mail {
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
+  }
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => personalize.addTo(recipient));
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    try {
+      const request = this.sgApi.emptyRequest({
+        method: "POST",
+        path: "/v3/mail/send",
+        body: this.toJSON()
+      });
+
+      const response = await this.sgApi.API(request);
+      return response;
+    } catch (error) {
+      console.log("error", error.response.body);
+    }
   }
 }
 
