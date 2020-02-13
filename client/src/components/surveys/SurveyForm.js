24793524c2a6dfa575bdbba0f29b9584fa2be0c2
pjcasas29 <pjcasas29@hotmail.com>
2020-02-13 13:23:36 -0500

    Typo

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -29,7 +29,7 @@ class SurveyForm extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(this.props.onSurveySumbit)}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat white-text">
             Cancel
