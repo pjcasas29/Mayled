32a88ecba7412fbd2f18e4d88d30a2dc6ce5f2c0
pjcasas29 <pjcasas29@hotmail.com>
2020-02-15 11:41:51 -0500

    Endpoint for returning surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,13 @@ const Survey = mongoose.model("surveys");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+    res.send(surveys);
+  });
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for Voting!");
   });
 
@@ -29,12 +35,13 @@ module.exports = app => {
           {
             _id: surveyId,
             recipients: {
-              $elemMatch: { mail: email, responded: false }
+              $elemMatch: { email: email, responded: false }
             }
           },
           {
             $inc: { [choice]: 1 },
-            $set: { "recipients.$.responded": true }
+            $set: { "recipients.$.responded": true },
+            lastResponded: new Date()
           }
         ).exec();
       })
